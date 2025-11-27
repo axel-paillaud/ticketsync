a0de979b50531719f9600a3b5626a96fbc07cfb4
Axel Paillaud <contact@axelweb.fr>
2025-11-27 15:58:26 +0100

    updatedAt can be nullable

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -25,7 +25,7 @@ class Ticket
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column]
+    #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'tickets')]
