d16e9e4e2d1ffb65c082a74fb6924bf2a25b91b0
Axel Paillaud <contact@axelweb.fr>
2025-11-29 11:22:06 +0100

    replace default homepage by ticket list

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/ticket')]
+#[Route('/')]
 final class TicketController extends AbstractController
 {
     #[Route(name: 'app_ticket_index', methods: ['GET'])]
