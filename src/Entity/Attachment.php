dc52a185576ca80f7bea671fdaa47016d9e2f808
Axel Paillaud <contact@axelweb.fr>
2025-12-06 20:02:19 +0100

    init attachment (without relation)

---
+++ src/Entity/Attachment.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AttachmentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AttachmentRepository::class)]
+class Attachment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $filename = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $storedFilename = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $mimeType = null;
+
+    #[ORM\Column]
+    private ?int $size = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $uploadedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): static
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getStoredFilename(): ?string
+    {
+        return $this->storedFilename;
+    }
+
+    public function setStoredFilename(string $storedFilename): static
+    {
+        $this->storedFilename = $storedFilename;
+
+        return $this;
+    }
+
+    public function getMimeType(): ?string
+    {
+        return $this->mimeType;
+    }
+
+    public function setMimeType(string $mimeType): static
+    {
+        $this->mimeType = $mimeType;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): static
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getUploadedAt(): ?\DateTimeImmutable
+    {
+        return $this->uploadedAt;
+    }
+
+    public function setUploadedAt(\DateTimeImmutable $uploadedAt): static
+    {
+        $this->uploadedAt = $uploadedAt;
+
+        return $this;
+    }
+}
