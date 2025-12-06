0e6dad0ff7bb6aedf8ffcc315ef3b35d50c7440f
Axel Paillaud <contact@axelweb.fr>
2025-12-06 20:38:13 +0100

    add Service to upload/move/delete file

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader
+{
+    public function __construct(
+        private string $targetDirectory,
+        private SluggerInterface $slugger,
+    ) {}
+
+    public function upload(UploadedFile $file): array
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $storedFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move($this->targetDirectory, $storedFilename);
+        } catch (FileException $e) {
+            throw new \RuntimeException('Failed to upload file: ' . $e->getMessage());
+        }
+
+        return [
+            'filename' => $file->getClientOriginalName(),
+            'storedFilename' => $storedFilename,
+            'mimeType' => $file->getMimeType(),
+            'size' => $file->getSize(),
+        ];
+    }
+
+    public function getTargetDirectory(): string
+    {
+        return $this->targetDirectory;
+    }
+
+    public function delete(string $storedFilename): void
+    {
+        $filePath = $this->targetDirectory . '/' . $storedFilename;
+
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
+    }
+}
