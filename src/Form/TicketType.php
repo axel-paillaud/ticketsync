11c1b7374e126d378eca7f1155a84329a56b539f
Axel Paillaud <contact@axelweb.fr>
2025-12-06 20:50:14 +0100

    add upload field in ticketType (form type)

--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -6,8 +6,10 @@ use App\Entity\Priority;
 use App\Entity\Ticket;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class TicketType extends AbstractType
 {
@@ -20,7 +22,33 @@ class TicketType extends AbstractType
                 'class' => Priority::class,
                 'choice_label' => 'name',
             ])
-        ;
+            ->add('attachments', FileType::class, [
+               'label' => 'Pièces jointes',
+               'mapped' => false,
+               'required' => false,
+               'multiple' => true,
+               'attr' => [
+                    'accept' => 'image/*,.pdf,.doc,.docx,.txt,.xlsx,.odt,.zip,.rar',
+               ],
+               'constraints' => [
+                    new File([
+                        'maxSize' => '10M',
+                        'mimeTypes' => [
+                            'image/*',
+                            'application/pdf',
+                            'application/msword',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                            'application/vnd.ms-excel',
+                            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                            'application/vnd.oasis.opendocument.text',
+                            'text/plain',
+                            'application/zip',
+                            'application/x-rar-compressed'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader un fichier valide (image, PDF, Word, Excel, texte, archive)',
+                    ])
+               ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
